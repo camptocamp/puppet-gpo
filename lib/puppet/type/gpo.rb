ef48a5fa0f10fe9a9961074ded9e1c2f6ff36a1c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-10-23 14:04:42 +0200

    Make ensurable

--- lib/puppet/type/gpo.rb
+++ lib/puppet/type/gpo.rb
@@ -1,4 +1,9 @@
 Puppet::Type.newtype(:gpo) do
+    ensurable do
+        defaultvalues
+        defaultto(:present)
+    end
+
     newparam(:path, :namevar => true) do
 
     end
--- spec/unit/puppet/type/gpo_spec.rb
+++ spec/unit/puppet/type/gpo_spec.rb
@@ -6,6 +6,20 @@ describe Puppet::Type.type(:gpo) do
             expect(described_class.key_attributes).to eq([:path])
         end
 
+        it 'should be ensurable' do
+            expect(described_class.attrtype(:ensure)).to eq(:property)
+        end
+
+        it 'should be ensured to present by default' do
+            res = described_class.new(:path => 'foo')
+            expect(res[:ensure]).to eq(:present)
+        end
+
+        it 'should be ensurable to absent' do
+            res = described_class.new(:path => 'foo', :ensure => :absent)
+            expect(res[:ensure]).to eq(:absent)
+        end
+
         it 'should have a value property' do
             expect(described_class.attrtype(:value)).to eq(:property)
         end
