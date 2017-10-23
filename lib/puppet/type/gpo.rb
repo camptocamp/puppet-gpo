ba5302ccdf769cb8f97b8567e5532caf019e6e13
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-10-23 14:00:42 +0200

    Add value property

--- lib/puppet/type/gpo.rb
+++ lib/puppet/type/gpo.rb
@@ -2,4 +2,8 @@ Puppet::Type.newtype(:gpo) do
     newparam(:path, :namevar => true) do
 
     end
+
+    newproperty(:value) do
+
+    end
 end
--- spec/unit/puppet/type/gpo_spec.rb
+++ spec/unit/puppet/type/gpo_spec.rb
@@ -5,5 +5,9 @@ describe Puppet::Type.type(:gpo) do
         it 'should have path as namevar' do
             expect(described_class.key_attributes).to eq([:path])
         end
+
+        it 'should have a value property' do
+            expect(described_class.attrtype(:value)).to eq(:property)
+        end
     end
 end
