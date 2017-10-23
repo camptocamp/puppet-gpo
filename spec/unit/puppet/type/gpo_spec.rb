d9b8cfb2c0489b119dd4204b9cee0ad1bec6da66
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-10-23 14:08:07 +0200

    Refactor specs

--- spec/unit/puppet/type/gpo_spec.rb
+++ spec/unit/puppet/type/gpo_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:gpo) do
-    context 'when validating attributes' do
+    context 'when validating namevar' do
         it 'should have path as namevar' do
             expect(described_class.key_attributes).to eq([:path])
         end
+    end
 
+    context 'when validating ensure' do
         it 'should be ensurable' do
             expect(described_class.attrtype(:ensure)).to eq(:property)
         end
@@ -19,7 +21,9 @@ describe Puppet::Type.type(:gpo) do
             res = described_class.new(:path => 'foo', :ensure => :absent)
             expect(res[:ensure]).to eq(:absent)
         end
+    end
 
+    context 'when validating value' do
         it 'should have a value property' do
             expect(described_class.attrtype(:value)).to eq(:property)
         end
