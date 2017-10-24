2221bf6dca639a0f94ac60768674d1fb390bd076
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-10-24 12:07:27 +0200

    Doc

--- lib/puppet/type/gpo.rb
+++ lib/puppet/type/gpo.rb
@@ -1,15 +1,22 @@
 require 'puppet_x/gpo'
 
 Puppet::Type.newtype(:gpo) do
+    @doc = <<-'EOT'
+    Apply a Windows local GPO.
+    EOT
+
     ensurable do
         defaultvalues
         defaultto(:present)
     end
 
     newparam(:path, :namevar => true) do
+        desc 'The GPO path, used for uniqueness.'
     end
 
     newparam(:scope, :namevar => true) do
+        desc 'The GPO scope.'
+
         newvalues(:user, :machine)
         defaultto(:machine)
 
@@ -19,18 +26,24 @@ Puppet::Type.newtype(:gpo) do
     end
 
     newparam(:admx_file, :namevar => true) do
+        desc 'The GPO ADMX File.'
+
         munge do |val|
             val.downcase
         end
     end
 
     newparam(:policy_id, :namevar => true) do
+        desc 'The GPO policy ID.'
+
         munge do |val|
             val.downcase
         end
     end
 
     newparam(:setting_valuename, :namevar => true) do
+        desc 'The GPO setting value name.'
+
         munge do |val|
             val.downcase
         end
@@ -81,6 +94,8 @@ Puppet::Type.newtype(:gpo) do
     end
 
     newproperty(:value) do
+        desc 'The GPO value.'
+
         validate do |val|
             scope = (resource[:scope] || :machine).to_s   # defaults get computed after validation
             path = resource[:path] || "#{scope}::#{resource[:admx_file]}::#{resource[:policy_id]}::#{resource[:setting_valuename]}"
